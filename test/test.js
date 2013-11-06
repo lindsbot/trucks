a60ecbbcd008089793d1dd47e64c61fdc87767d6
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-11-06 12:40:47 -0800

    adding node tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,26 @@
 var request = require('superagent');
 var expect = require('chai').expect;
-var should = require('chai').should;
 
 describe('trucks server', function(){
   it('returns true', function(){
     return true;
   });
+
+
+
+
 });
 
 
-// expect(function(done){
-//     request.post('localhost:3000').end(function(res){
-//       expect(res).to.exist;
-//       expect(res.status).to.equal(200);
-//       // TODO: add more server tests
-//       //expect(res.body).to.contain();
-//       done();
-//     });
-//   });
+  // it('test test', function(){
+  //     expect(function(done){
+  //       request.post('localhost:3000').end(function(res){
+  //       expect(res).to.exist;
+  //       expect(res.status).to.equal(200);
+  //       expect(res.body).to.contain('LSDJKFGLSKDJGL');
+  //       // TODO: add more server tests
+  //       //expect(res.body).to.contain();
+  //       done();
+  //     });
+  //   });
+  // })
