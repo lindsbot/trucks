2824b6ef78cb3f7b2a129b6ab2e66499e93e7f2a
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-11-06 13:53:50 -0800

    removed jade, added backbone

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'public/views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -22,15 +22,15 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if (app.get('env') === 'development') {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('*', function(req, res){
+  res.send('index.html');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
