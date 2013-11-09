c08abc15adce30a86ae08b0e37b7ae93cdc5b142
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-11-08 17:53:43 -0800

    code cleanup

--- public/js/views/appView.js
+++ public/js/views/appView.js
@@ -2,8 +2,10 @@ var AppView = Backbone.View.extend({
   initialize: function() {
     this.mapView = new MapView({model: new Map()});
     this.mapView.render();
+
     this.trucksView = new TrucksView({collection: this.model.filteredTrucks});
     this.trucksView.render();
+
     this.listView = new ListView({collection: this.model.filteredTrucks});
     this.listView.render();
 
@@ -25,6 +27,7 @@ var AppView = Backbone.View.extend({
       var latLng = new google.maps.LatLng(lat,lng);
       if (bounds.contains(latLng)) { return true; }
     });
+    
     this.trucksView.collection.reset(updatedTrucks);
     this.listView.render();
   }
